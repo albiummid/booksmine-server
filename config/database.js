e3808f070afce16d56b3b982cdfedaca2be7d3e8
albiummid <albi.ummid@gmail.com>
2022-02-04 13:47:17 +0600

    mongoDB connected

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require( 'mongoose' )
+
+const connectDatabase = () =>
+{
+    mongoose
+        .connect( process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        } )
+        .then( ( con ) =>
+        {
+            console.log(
+                `MongoDB Database Connected with HOST: ${con.connection.host}`
+            )
+        } )
+}
+
+module.exports = connectDatabase
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+const app = require( './app' );
+const dotenv = require( 'dotenv' );
+const connectDatabase = require( './config/database' );
 
 // setting up config file
-dotenv.config({path:'./config/config.env'})
+dotenv.config( { path: './config/config.env' } )
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server started on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
-})
+// Connecting To DATABASE
+connectDatabase();
+
+
+app.listen( process.env.PORT, () =>
+{
+    console.log( `Server started on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode.` );
+} )
