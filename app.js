0a8afab8d719d019dc977b00de93dcc09bfc495a
Albi Ummid <albi.ummid@gmail.com>
2022-02-13 10:10:11 +0600

    cross origin fix

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const express = require( 'express' );
-const books = require( './routes/book' );
-const department = require( './routes/department' );
-const semester = require( './routes/semester' );
-const course = require( './routes/course' );
-const order = require( './routes/order' )
-const cors = require( 'cors' )
+const express = require('express')
+const books = require('./routes/book')
+const department = require('./routes/department')
+const semester = require('./routes/semester')
+const course = require('./routes/course')
+const order = require('./routes/order')
+const cors = require('cors')
+const allowCrossDomain = require('./utils/cors')
 
-const app = express();
+const app = express()
 
-app.use( express.json() )
-app.use( express.urlencoded( { extended: false } ) )
-app.use( cors() )
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(allowCrossDomain())
 
 // Home Page of BOOKIMINE server
 
-app.get( '/', ( req, res ) =>
-{
-    res.send( `Hola ! You are on official Booksmine Node.js Server PORT-${process.env.PORT} in ${process.env.NODE_ENV} !` );
-} );
-
-
+app.get('/', (req, res) => {
+  res.send(
+    `Hola ! You are on official Booksmine Node.js Server PORT-${process.env.PORT} in ${process.env.NODE_ENV} !`
+  )
+})
 
 // Use all routes
-app.use( '/api/v1/book', books );
-app.use( '/api/v1/department', department );
-app.use( '/api/v1/semester', semester )
-app.use( '/api/v1/course', course )
-app.use( '/api/v1/order', order )
+app.use('/api/v1/book', books)
+app.use('/api/v1/department', department)
+app.use('/api/v1/semester', semester)
+app.use('/api/v1/course', course)
+app.use('/api/v1/order', order)
 
-module.exports = app;
+module.exports = app
