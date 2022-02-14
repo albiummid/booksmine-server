2d05abd33a6837dda4cce649697a7db5767ca0df
Albi Ummid <albi.ummid@gmail.com>
2022-02-14 18:34:36 +0600

    role stringed on user

--- models/user.js
+++ models/user.js
@@ -19,7 +19,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    role: Number,
+    role: {
+      type: String,
+      required: true,
+    },
     userSettings: {
       base: {
         type: String,
