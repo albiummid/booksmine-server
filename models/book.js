ebd3dda73413bef2a32d1b7dee66945b207d1cf8
albiummid <albiummid@gmail.com>
2022-02-26 15:51:19 +0600

    book model updated

--- models/book.js
+++ models/book.js
@@ -1,68 +1,87 @@
-const mongoose = require( 'mongoose' );
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const bookSchema = new Schema( {
+const bookSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: [ true, 'Please enter book title' ],
-        trim: true,
-        maxLength: [ 50, 'Title cannot exceed 50 charecters' ]
+      type: String,
+      required: [true, 'Please enter book title'],
+      trim: true,
+      maxLength: [50, 'Title cannot exceed 50 charecters'],
     },
     author: {
-        type: String,
-        required: [ true, 'Please enter author name' ],
-    },
-    department: {
-        type: String,
-        required: [ true, 'Please enter department   ' ],
+      type: String,
+      required: [true, 'Please enter author name'],
     },
     imgUrl: {
-        type: String,
-        required: [ true, 'Please give image URL  ' ],
-    }
-    ,
-    semester: {
-        type: String,
-        required: [ true, 'Please enter semester' ],
+      type: String,
+      required: [true, 'Please give image URL  '],
     },
-    courseCode: {
-        type: String,
-        required: [ true, 'Please enter course code' ],
-    }
-    ,
     ratings: {
-        type: Number,
-        required: false,
-        default: 0,
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    originalPrice: {
+      type: Number,
+      required: true,
     },
     price: {
-        type: Number,
-        required: [ true, 'Please enter price ' ],
+      type: Number,
+      required: [true, 'Please enter price '],
     },
     discount: {
-        type: Number,
-        required: [ true, 'Please enter discount ' ],
+      type: Number,
+      required: [true, 'Please enter discount '],
+    },
+    discountDuration: {
+      type: Object,
+      startAt: {
+        type: String,
+        required: true,
+      },
+      endAt: {
+        type: String,
+        required: true,
+      },
     },
     inStock: {
-        type: Boolean,
-        required: [ true, 'Please enter stock ' ],
+      type: Number,
+      required: [true, 'Please enter stock '],
+    },
+    isStock: {
+      type: Boolean,
+      required: [true, 'Please enter stock '],
     },
     reviews: [
-        {
-            user: {
-                type: String,
-                required: [ true, 'Please give reviewer name' ]
-            },
-            rating: {
-                type: Number,
-                required: [ true, 'Please give ratings' ]
-            }
-        }
+      {
+        user: {
+          type: String,
+          required: [true, 'Please give reviewer name'],
+        },
+        rating: {
+          type: Number,
+          required: [true, 'Please give ratings'],
+        },
+      },
+    ],
+    department: {
+      type: String,
+      required: [true, 'Please enter department   '],
+    },
 
-    ]
+    semester: {
+      type: String,
+      required: [true, 'Please enter semester'],
+    },
+    courseCode: {
+      type: String,
+      required: [true, 'Please enter course code'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-}, {
-    timestamps: true
-} )
-
-module.exports = mongoose.model( 'Book', bookSchema );
+module.exports = mongoose.model('Book', bookSchema)
