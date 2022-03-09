10500f8e3d04f6968fdd95f5f83d3d85051c2c96
albiummid <albiummid@gmail.com>
2022-03-10 01:10:45 +0600

    added

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const role = require('./routes/roleRoutes')
 
 const errorMiddleware = require('./middlewares/error')
 const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const multer = require('multer')
 
 const app = express()
 const allowCrossDomain = function (req, res, next) {
@@ -24,7 +22,7 @@ const allowCrossDomain = function (req, res, next) {
   next()
 }
 
-// app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(
   cors({
