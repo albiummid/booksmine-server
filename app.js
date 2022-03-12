7a129ce83d6c32809bb2ccb2cad3b23d8a3d0a52
albiummid <albiummid@gmail.com>
2022-03-12 10:36:06 +0600

    a

--- app.js
+++ app.js
@@ -35,19 +35,19 @@ const allowCrossDomain = function (req, res, next) {
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   )
-//   if (req.method == 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-//     return res.status(200).json({})
-//   }
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
 
-//   next()
-// })
+  next()
+})
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', '*')
