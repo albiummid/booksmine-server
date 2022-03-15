4c5493e37faa926fd241c039693b8567675e365a
albiummid <albiummid@gmail.com>
2022-03-15 10:54:21 +0600

    cors

--- app.js
+++ app.js
@@ -21,23 +21,35 @@ const allowCrossDomain = function (req, res, next) {
 
   next()
 }
-app.use(allowCrossDomain)
+// app.use(allowCrossDomain)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    'Origin, X-Requested-With,Content-Type,Accept,Authorization'
   )
-  if (req.method == 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-    return res.status(200).json({})
-  }
-
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,PATCH,POST,DELETE,OPTIONS'
+  )
   next()
 })
+// app.use(cors())
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   )
+//   if (req.method == 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+//     return res.status(200).json({})
+//   }
+
+//   next()
+// })
 
 app.get('/', (req, res) => {
   res.send(
